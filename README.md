4ba5f5bb301b618989ea57be0fd3b4f66e47bd3d
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-07-03 19:26:11 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# mutago
-ID3 tagger for Go
+# Mutago 
+> ### Simple ID3 tagger for Go 
+
+
+## License
+
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/LGPLv3_Logo.svg/1200px-LGPLv3_Logo.svg.png" alt="license" height="40%" width="40%">
+
+
+
+
---
+++ utils.go
@@ -0,0 +1,27 @@
+package mutago
+
+import (
+	"errors"
+	"os"
+)
+
+func ID3Version(f *os.File) (int, error) {
+	/*
+		Returns ID3 version.
+
+		ID3v1 = 1
+		ID3v2 = 2
+		No ID3 tag found = error
+	*/
+
+	b := make([]byte, 3)
+	stat, _ := f.Stat() // Get file stats for file size
+
+	if f.ReadAt(b, 0); string(b) == "ID3" {
+		return 2, nil // ID3v2 has 'ID3' in first 3 bytes
+	} else if f.ReadAt(b, stat.Size()-128); string(b) == "TAG" {
+		return 1, nil // ID3v1 has 'TAG' in first 3 bytes in last 128 bytes
+	}
+
+	return -1, errors.New("ID3 Tags : None found")
+}
