3e87855990fe9b94be0f273fcf9c05968dd7d6c5
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-07-08 16:20:18 +0530

    Updated example. Reading tags

--- examples/example.go
+++ examples/example.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/makebyte/mutago/v2"
+	"../v2"
 )
 
 func main() {
 	f, _ := os.Open("ok.mp3")
 	z := v2.ParseHeader(f)
-	fmt.Println(z.Size)
+	r, _ := f.Seek(0, 1)
+	for int(r) < int(z.Size) {
+		x := v2.ParseFrame(f)
+		fmt.Println(x.ID)
+		fmt.Println(x.Info)
+		r, _ = f.Seek(0, 1)
+		fmt.Println("\n\n")
+	} // Print all tags
+
+	/*
+		Note after APIC there might be junk values since they're part of
+		the album art
+	*/
 
 }
