d952f0f0a6840033c73cd61e8bdacbba3e108938
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-07-09 13:20:37 +0530

    Updated examples

--- examples/example.go
+++ examples/example.go
@@ -7,21 +7,23 @@ import (
 )
 
 func main() {
-	f, err := mutago.Open("ok.mp3")
+	f, err := mutago.Open("sample.mp3")
 	defer f.Close()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	val, err := f.Get("TIT2") // Get title value
-	if err != nil {
-		fmt.Println(err)
-	}
+	title, _ := f.Title() // Get title value
+	album, _ := f.Album()
+	artist, _ := f.Artist()
 
-	fmt.Println(val)
+	fmt.Println(title, album, artist)
 
-	tags := f.List() // Get all available tags (Not values)
+	tags := f.List() // Get all available tags identifiers (Not values)
 	for _, y := range tags {
-		fmt.Println(y) // Print all tags
+		fmt.Println(y) // Print identifiers
 	}
+
+	text, _ := f.Get("TXXX") // Getting tag with identifier TXXX
+	fmt.Println(text)
 }
