77ed85c68b52db500da1e605584199f19a08b6d7
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-07-11 23:42:37 +0530

    Checking v2 before v1 in ID3Version()

--- utils.go
+++ utils.go
@@ -16,11 +16,11 @@ func ID3Version(f *os.File) (int, error) {
 
 	b := make([]byte, 3)
 	stat, _ := f.Stat() // Get file stats for file size
-
-	if f.ReadAt(b, stat.Size()-128); string(b) == "TAG" {
-		return 1, nil // ID3v1 has 'TAG' in first 3 bytes in last 128 bytes
-	} else if f.ReadAt(b, 0); string(b) == "ID3" {
+	if f.ReadAt(b, 0); string(b) == "ID3" {
 		return 2, nil // ID3v2 has 'ID3' in first 3 bytes
+	} else if f.ReadAt(b, stat.Size()-128); string(b) == "TAG" {
+		return 1, nil // ID3v1 has 'TAG' in first 3 bytes in last 128 bytes
 	}
 	return 0, errors.New("ID3 Tags : None found")
+
 }
