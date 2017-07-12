453cc9b743dc38f95ceed1cdb74a805ff79c998f
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-07-12 18:08:57 +0530

    Updated examples (Added albumart example)

--- examples/example.go
+++ examples/example.go
@@ -1,29 +1,48 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/makebyte/mutago"
 )
 
 func main() {
+	fmt.Println("\n")
+
 	f, err := mutago.Open("sample.mp3")
 	defer f.Close()
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
-	title, _ := f.Title() // Get title value
-	album, _ := f.Album()
-	artist, _ := f.Artist()
+	/* Basic methods (Title, album, artist, album art) */
+	title, _ := f.Title()   // string
+	album, _ := f.Album()   // string
+	artist, _ := f.Artist() // string
 
-	fmt.Println(title, album, artist)
+	/* albumart struct containing:
+	   - Mime (format of image) (string)
+	   - TextEncoding (encoding of Description) (Byte)
+	   - Description (Description of cover) (string)
+	   - PictureType (According to ID3) (Byte)
+	*/
+	albumart := f.Albumart()
 
-	tags := f.List() // Get all available tags identifiers (Not values)
-	for _, y := range tags {
-		fmt.Println(y) // Print identifiers
-	}
+	reader := bytes.NewReader(albumart.Data)
+	img, _ := os.Create("albumart.png")
+	defer img.Close()
 
-	text, _ := f.Get("TXXX") // Getting tag with identifier TXXX
-	fmt.Println(text)
+	io.Copy(img, reader)
+
+	fmt.Println("Song title :", title)
+	fmt.Println("Song album : ", album)
+	fmt.Println("Song artist : ", artist)
+	fmt.Println("Albumart image type : ", albumart.Mime)
+	fmt.Println("Albumart description : ", albumart.Description)
+
+	fmt.Println("\n\nSaved albumart.png")
+
 }
