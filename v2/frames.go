14be9f87ff71ac961859db26077a7c15e23b516c
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-07-12 17:55:56 +0530

    Parsing albumart in v2/frames

--- v2/frames.go
+++ v2/frames.go
@@ -16,6 +16,14 @@ type Frame struct {
 	Info string
 }
 
+type Albumart struct {
+	TextEncoding byte
+	Mime         string
+	PictureType  byte
+	Description  string
+	Data         []byte
+}
+
 func ParseFrame(reader io.ReadSeeker) *Frame {
 
 	header := make([]byte, FrameHeaderSize)
@@ -39,4 +47,42 @@ func ParseFrame(reader io.ReadSeeker) *Frame {
 	frame.Info = string(info)
 
 	return frame
+}
+
+func ParseAlbumart(info string) *Albumart {
+	/*
+	   Returns the image in byte array, the image type and error
+	*/
+
+	var null byte = 0
+	data := []byte(info)
+
+	albumart := &Albumart{}
+	albumart.TextEncoding = data[0] // First byte signifies text encoding
+
+	for i, val := range data {
+		if val == null {
+			albumart.Mime = string(data[:i])
+			data = data[i+1:]
+			break
+		}
+	}
+
+	albumart.PictureType = data[0]
+
+	for i, val := range data {
+		if val == null {
+			albumart.Description = string(data[:i])
+			data = data[i+1:]
+			break
+		}
+	}
+
+	for data[0] == null {
+		data = data[1:]
+	}
+
+	albumart.Data = data
+
+	return albumart
 }
